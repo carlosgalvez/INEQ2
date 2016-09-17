c001c76c2cdbc6c7d0b0aa0e00ebf540f3baca12
carlosgalvez <carlosgalvez9707@hotmail.com>
2016-09-17 15:28:00 -0500

    1

---
+++ IneqApi/IneqApi/Controllers/EquipmentTypeController.cs
@@ -0,0 +1,50 @@
+﻿using IneqApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace IneqApi.Controllers
+{
+   
+    public class EquipmentTypeController : ApiController
+    {
+         private IneqContext db = new IneqContext();
+        // GET api/equipmenttype
+        public List<EquipmentType> Get()
+        {
+            return db.EquipmentTypes.ToList();
+        }
+
+        // GET api/equipmenttype/5
+        public List<EquipmentType> Get(int Id)
+        {
+            return db.EquipmentTypes.Where(e => e.Id == Id).ToList();
+        }
+
+        // POST api/equipmenttype
+        public bool Post(int Id, string Description, string UsefulLife, string GuaranteeDuration, int Active)
+        {
+
+            var e= new EquipmentType{
+                Id = Id,
+                Description=Description,
+                UsefulLife=U
+
+            }
+        }
+
+        // PUT api/equipmenttype/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/equipmenttype/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
