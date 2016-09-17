fa703edfc6b2345dc8ed0a679f3881f11d373683
carlosgalvez <carlosgalvez9707@hotmail.com>
2016-09-17 08:42:32 -0500

    CAGCH Se subio las clases

---
+++ IneqApi/IneqApi/Models/IneqContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace IneqApi.Models
+{
+    public class IneqContext:DbContext
+    {
+
+
+        public IneqContext():base("IContext")
+        {     
+        }
+        public DbSet<Equipment> Equipments { get; set; }
+        public DbSet<EquipmentType> EquipmentTypes { get; set; }
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+
+}
